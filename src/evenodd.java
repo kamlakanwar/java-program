2819f04e4122e7ee4f52cc762c65f5eb44e9a553
kamlakanwar <kamlakanwar923@gmail.com>
2024-02-13 22:14:18 -0500

    evenodd program

--- src/evenodd.java
+++ src/evenodd.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
 public class evenodd {
    public static void main(String[] args) {
-    if (n%2==0) {
+    System.out.println("enter a value");
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    if (n % 2==0) {
         System.out.println("number is even");
     }
     else {
-        System.out.println("number is false");
+        System.out.println("number is add");
     }
    } 
 }
