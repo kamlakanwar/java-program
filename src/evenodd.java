ad913f3744ce517a73343280f0f096936ce88760
kamlakanwar <kamlakanwar923@gmail.com>
2024-02-13 22:10:08 -0500

    evenodd program

---
+++ src/evenodd.java
@@ -0,0 +1,10 @@
+public class evenodd {
+   public static void main(String[] args) {
+    if (n%2==0) {
+        System.out.println("number is even");
+    }
+    else {
+        System.out.println("number is false");
+    }
+   } 
+}
