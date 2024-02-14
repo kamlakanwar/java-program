5aea438b36f3081ed227343d3531c0c757deeeec
kamlakanwar <kamlakanwar923@gmail.com>
2024-02-13 20:22:19 -0500

    conitional program

---
+++ src/cond.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class cond {
+    public static void main(String[]args) {
+        System.out.println("enter your age");
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        if (age < 18) {
+            System.out.println("you are eligible for vote");
+        }
+            else {
+                System.out.println("you are not eligible for vote");
+            }
+        }
+    }
