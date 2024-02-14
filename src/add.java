36f9cb52c9b6e0a145d5b718c6562cf2098301df
kamlakanwar <kamlakanwar923@gmail.com>
2024-02-13 19:54:43 -0500

    java addition

---
+++ src/add.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class add{
+    public static void main (String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the value of a and b");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
