2b334ad050a1d928c747d3c61b0c29d53d8867e3
kamlakanwar <kamlakanwar923@gmail.com>
2024-02-16 15:39:48 -0500

    natural number

---
+++ src/loops.java
@@ -0,0 +1,13 @@
+public class loops {
+    public static void main(String[]args){
+        int i=0;
+        do{
+            System.out.println("hello");
+            i++;
+            System.out.println("world"); 
+        }
+        while(i<=10);
+        
+    }
+    
+}
